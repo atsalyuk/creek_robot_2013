f00756b7a5b9ac0d2171ea583da79971249e2d7a
Pherion22 <hhaase12@gmail.com>
2013-02-07 21:40:43 -0700

    Added Talon Class

---
+++ src/com/cc/outputs/motor/CCTalon.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cc.outputs.motor;
+
+import edu.wpi.first.wpilibj.Talon;
+
+/**
+ *
+ * @author Henry
+ */
+public class CCTalon extends Talon
+{
+    
+    private boolean reversed = false;
+    
+    public CCTalon ( int channel, boolean direction )
+    {
+        super( channel );
+        reversed = direction;
+    }
+    
+    public void set( double speed )
+    {
+        if( reversed )
+        {
+            super.set ( speed * -1.0 );
+        }
+        else
+        {
+            super.set( speed );
+        }
+    }
+}
