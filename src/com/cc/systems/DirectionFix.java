ec3e7f89790f6ba31941b36d4f8d602e8dcefafa
zombiehobbit <dmanhall@gmail.com>
2013-02-09 14:58:23 -0700

    added to direction fix class

--- src/com/cc/systems/DirectionFix.java
+++ src/com/cc/systems/DirectionFix.java
@@ -8,6 +8,22 @@ package com.cc.systems;
  *
  * @author dillon
  */
-public class DirectionFix {
+public class DirectionFix 
+{
+  private DirectionFix INSTANCE;
+  
+  private DirectionFix()
+  {
+      
+  }
+  
+  public DirectionFix getInstance()
+  {
+      if(INSTANCE==null)
+          INSTANCE=new DirectionFix();
+      
+      return INSTANCE;
+  }
+    
     
 }
