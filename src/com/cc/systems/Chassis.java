9ab54c542e65b6981804bf55b394f321a2fce9b2
Adam Bryant <bryant.adam@gmail.com>
2013-03-09 12:35:23 -0700

    Implemented turnAngle() method using P and D terms

--- src/com/cc/systems/Chassis.java
+++ src/com/cc/systems/Chassis.java
@@ -5,13 +5,10 @@
 package com.cc.systems;
 
 import com.cc.outputs.motor.CCTalon;
-import com.cc.outputs.motor.CCVictor;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Watchdog;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.Ultrasonic;
 
 /**
  *
@@ -25,12 +22,9 @@ public class Chassis
     CCTalon rightMotor1 = null;
     Encoder encoder = null;
     Gyro gyro = null;
-
     private final static double TICKSPERINCH = 15.43; //For the Competition Robot
     private final static double Kp = 0.30;
-   
-    
-    
+
     private Chassis()
     {
         leftMotor1 = new CCTalon( 4, false );
@@ -125,7 +119,7 @@ public class Chassis
         while ( Math.abs( encoder.get() ) < ticksToTravel )
         {
             double angle = gyro.getAngle();
-            
+
             this.drive( -angle * Kp, speed );
             Timer.delay( 0.05 );
 //            System.out.println( "Encoder: " + encoder.get() );
@@ -152,51 +146,81 @@ public class Chassis
             return val;
         }
     }
-    
+
+//    public void turnAngle( double angleToTurn, double speed )
+//    {
+//        boolean done = false;
+//        double tKp = 0.025;
+//        double Ki = 0.002;
+//        double errorSum = 0.0;
+//        Timer timer = new Timer();
+//
+//        gyro.reset();
+//        timer.start();
+//
+//        while (!done)
+//        {
+//            double error = angleToTurn - gyro.getAngle();
+//
+//            errorSum += error;
+//
+//            double xSpeed = maxNormalize( error * tKp /*+ errorSum * Ki*/ , speed );
+//
+//            this.drive( xSpeed , 0.0);
+//
+////            if ( Math.abs( xSpeed ) < 0.10 && Math.abs( error ) < 1.0 )
+////            {
+////                done = true;
+////            }
+//            if (timer.get() > 5)
+//                done = true;
+//
+//            System.out.println("Timer: " + timer.get());
+////            System.out.println( "Speed: " + xSpeed + " Error: " + error);
+//            Timer.delay( 0.1 );
+//        }
+//
+//        timer.stop();
+//
+////        while ( Math.abs( gyro.getAngle() ) < angleToTurn )
+////        {
+////            this.drive( speed, 0.0 );
+////        }
+//
+//        this.stop();
+//        System.out.println( "Angle of Robot: " + gyro.getAngle() );
+//
+//        gyro.reset();
+//    }
     public void turnAngle( double angleToTurn, double speed )
     {
         boolean done = false;
-        double tKp = 0.025;
-        double Ki = 0.002;
-        double errorSum = 0.0;
-        Timer timer = new Timer();
-        
-        gyro.reset();
-        timer.start();
-        
-        while (!done)
-        {
-            double error = angleToTurn - gyro.getAngle();
-            
-            errorSum += error;
-            
-            double xSpeed = maxNormalize( error * tKp /*+ errorSum * Ki*/ , speed );
-            
-            this.drive( xSpeed , 0.0);
-            
-//            if ( Math.abs( xSpeed ) < 0.10 && Math.abs( error ) < 1.0 )
-//            {
-//                done = true;
-//            }
-            if (timer.get() > 5)
-                done = true;
+        double kP = 0.025;
+        double kD = 0.01;
+        double error = 0.0;
+        double prevError = 0.0;
 
-            System.out.println("Timer: " + timer.get());
-//            System.out.println( "Speed: " + xSpeed + " Error: " + error);
-            Timer.delay( 0.1 );
+        gyro.reset();
+
+        while ( !done )
+        {
+            prevError = error;
+            error = normalize( (angleToTurn - gyro.getAngle()) / 100.0 );
+
+            double p = error * kP;
+            double d = (error - prevError) * kD;
+
+            this.drive( p + d, 0.0 );
+
+            if ( Math.abs( error ) < 0.01 )
+            {
+                done = true;
+            }
+
+            Timer.delay( 0.05 );
         }
 
-        timer.stop();
-        
-//        while ( Math.abs( gyro.getAngle() ) < angleToTurn )
-//        {
-//            this.drive( speed, 0.0 );
-//        }
-
         this.stop();
-        System.out.println( "Angle of Robot: " + gyro.getAngle() );
-
-        gyro.reset();
     }
 
     public void driveSonar( double speed, double distanceToTravel, boolean useFeet )
